2e806d3d19991bc170db971a85d79ec7cf0f9933
Frai Vadim <70962544+FraiVadim@users.noreply.github.com>
2020-11-26 10:30:52 +0300

    Add files via upload

---
+++ str.probleama_3.py
@@ -0,0 +1,6 @@
+s1=str(input("Introduceti primul cuvant exprimat printro variabila: "))
+s2=str(input("Introduceti al doilea cuvant exprimat printro variabila: " ))
+s3=str(input("Introduceti al treilea cuvant exprimat printro variabila: "))
+s4=str(input("Introduceti al patrulea cuvant exprimat printro variabila: "))
+print("Fraza formata din aceste cuvinte este: {0} {1} {2} {3}" .format(s1, s2, s3, s4 ),".")
+
